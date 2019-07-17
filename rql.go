48d898d71bd7fd32cea9b2f253e25aa8e61360c3
ashtonian <github@ashtonkinslow.com>
2019-07-17 15:08:29 -0500

    add ParseQuery to allow web frameworks to parse the initial json contents

--- rql.go
+++ rql.go
@@ -154,6 +154,12 @@ func (p *Parser) Parse(b []byte) (pr *Params, err error) {
 	}()
 	q := new(Query)
 	must(q.UnmarshalJSON(b), "decoding buffer to Query")
+	return p.ParseQuery(*q)
+}
+
+// Parse Query parses the given struct into a Param object. It returns an error
+// if the filter JSON is invalid, or its values don't follow the schema of rql.
+func (p *Parser) ParseQuery(q Query) (pr *Params, err error) {
 	pr = &Params{
 		Limit: p.DefaultLimit,
 	}
