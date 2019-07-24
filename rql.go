053b06acb6d584cc25bdafa8a23fb26c0dc0352d
Ashton Kinslow <Ashtonian@users.noreply.github.com>
2019-07-24 14:48:09 -0500

    Update rql.go
    Co-Authored-By: Ariel Mashraki <7413593+a8m@users.noreply.github.com>

--- rql.go
+++ rql.go
@@ -159,7 +159,7 @@ func (p *Parser) Parse(b []byte) (pr *Params, err error) {
 	return p.ParseQuery(*q)
 }
 
-// Parse Query parses the given struct into a Param object. It returns an error
+// ParseQuery parses the given struct into a Param object. It returns an error
 // if the filter JSON is invalid, or its values don't follow the schema of rql.
 func (p *Parser) ParseQuery(q Query) (pr *Params, err error) {
 	defer func() {
